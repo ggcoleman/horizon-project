3e40893a0f3a06df59ea59f6b96dec529b15b7e5
Guy Coleman <coleman.guyg@gmail.com>
2022-01-02 16:21:18 +0200

    Rocket model, ground and launch and landing pad

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,14 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        ProcessInput();
+    }
 
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Thrusting....");
+        }
     }
 }
